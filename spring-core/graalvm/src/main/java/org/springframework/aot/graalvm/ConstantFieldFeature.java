a075cf4a3d2f0a0240fac3345574a88700327b2c
Sébastien Deleuze <sdeleuze@vmware.com>
2022-09-05 13:00:59 +0200

    Reimplement ConstantFieldFeature with FieldValueTransformer

--- spring-core/graalvm/src/main/java/org/springframework/aot/graalvm/ConstantFieldFeature.java
+++ spring-core/graalvm/src/main/java/org/springframework/aot/graalvm/ConstantFieldFeature.java
@@ -16,35 +16,71 @@
 
 package org.springframework.aot.graalvm;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.regex.Pattern;
+
 import com.oracle.svm.core.annotate.AutomaticFeature;
-import com.oracle.svm.hosted.FeatureImpl.DuringSetupAccessImpl;
-import org.graalvm.compiler.debug.DebugContext;
+
 import org.graalvm.nativeimage.hosted.Feature;
 
 /**
  * GraalVM {@link Feature} that substitutes field values that match a certain pattern
  * with constants without causing build-time initialization.
  *
- * @author Phillip Webb
  * @author Sebastien Deleuze
  * @since 6.0
  */
 @AutomaticFeature
 class ConstantFieldFeature implements Feature {
 
+	// TODO Should we keep those patterns or use an annotation?
+	private static Pattern[] patterns = {
+			Pattern.compile(Pattern.quote("org.springframework.core.NativeDetector#imageCode")),
+			Pattern.compile(Pattern.quote("org.springframework.") + ".*#.*Present"),
+			Pattern.compile(Pattern.quote("org.springframework.") + ".*#.*PRESENT"),
+			Pattern.compile(Pattern.quote("reactor.") + ".*#.*Available")
+	};
+
+	private final ThrowawayClassLoader throwawayClassLoader = new ThrowawayClassLoader(ConstantFieldFeature.class.getClassLoader());
+
 	@Override
-	public void duringSetup(DuringSetupAccess access) {
-		duringSetup((DuringSetupAccessImpl) access);
+	public void beforeAnalysis(BeforeAnalysisAccess access) {
+		access.registerSubtypeReachabilityHandler(this::iterateFields, Object.class);
 	}
 
-	private void duringSetup(DuringSetupAccessImpl access) {
-		DebugContext debug = access.getDebugContext();
-		try (DebugContext.Scope scope = debug.scope("ConstantFieldFeature.duringSetup")) {
-			debug.log("Installing constant field substitution processor : " + scope);
-			ClassLoader classLoader = ConstantFieldFeature.class.getClassLoader();
-			ConstantFieldSubstitutionProcessor substitutionProcessor =
-					new ConstantFieldSubstitutionProcessor(debug, classLoader);
-			access.registerSubstitutionProcessor(substitutionProcessor);
+	/* This method is invoked for every type that is reachable. */
+	private void iterateFields(DuringAnalysisAccess access, Class<?> subtype) {
+		try {
+			for (Field field : subtype.getDeclaredFields()) {
+				String fieldIdentifier = field.getDeclaringClass().getName() + "#" + field.getName();
+				for (Pattern pattern : patterns) {
+					int modifiers = field.getModifiers();
+					if (pattern.matcher(fieldIdentifier).matches() &&
+							Modifier.isStatic(modifiers) &&
+							Modifier.isFinal(modifiers) &&
+							!field.isEnumConstant()
+					) {
+						System.out.println("Field " + fieldIdentifier + " set to false at build time");
+						access.registerFieldValueTransformer(field, (receiver, originalValue) -> provideFieldValue(field));
+					}
+				}
+			}
+		} catch (NoClassDefFoundError ex) {
+			// Skip classes that have not all their field types in the classpath
+		}
+	}
+
+	/* This method is invoked when the field value is written to the image heap or the field is constant folded. */
+	private Object provideFieldValue(Field field) {
+		try {
+			Class<?> throwawayClass = this.throwawayClassLoader.loadClass(field.getDeclaringClass().getName());
+			Field throwawayField = throwawayClass.getDeclaredField(field.getName());
+			throwawayField.setAccessible(true);
+			return throwawayField.get(null);
+		}
+		catch (Exception ex) {
+			throw new IllegalStateException("Unable to read value from " + field.getDeclaringClass().getName() + "." + field.getName(), ex);
 		}
 	}
 
